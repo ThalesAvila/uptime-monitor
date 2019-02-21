418543803686088d42831f0309ce8b6ff3f8beb2
Thales Avila <thalespizza@gmail.com>
2019-02-20 23:44:06 -0300

    Parsing headers

--- index.js
+++ index.js
@@ -14,18 +14,21 @@ const server = http.createServer((req, res) => {
   // Get path
   const path = parsedUrl.pathname
   const trimmedPath = path.replace(/^\/+|\/+$/g,'')
+  
+  // Get a query string como um objeto
+  const queryStringObject = parsedUrl.query
 
   // Get método HTTP
   const method = req.method.toLowerCase()
 
-  // Get a query string como um objeto
-  const queryStringObject = parsedUrl.query
-
+  // Get os headers como um objeto
+  const headers = req.headers 
   // Mandar response
   res.end('Hello World!\n')
 
   // Log o request path
   console.log(`Request recebido no path: ${trimmedPath} com o método: ${method} e com esses parâmetros query string`, queryStringObject)
+  console.log(`Requisição recebida com os seguintes headers:`, headers)
   
 })
 
