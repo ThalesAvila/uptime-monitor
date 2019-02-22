25477e5a663da96e548a1adf44e4be9c513485b2
Thales Avila <thalespizza@gmail.com>
2019-02-21 23:05:23 -0300

    Roteando as requisiçoes

--- index.js
+++ index.js
@@ -33,19 +33,62 @@ const server = http.createServer((req, res) => {
   })
   req.on('end', () => {
     buffer += decoder.end()
-    // Mandar response
-    res.end('Hello World!\n')
 
-    // Log o request path
-    console.log(`Requisição recebida com o seguinte payload:`, buffer)
+    // Definir o handler para qual o request deverá ir. Se o handler não for encontrado, usar o handler notFound
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound
+
+    //Construir o data object para enviar ao handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      'payload' : buffer
+    }
+
+    // Rotear a requisição para o handler especificado no router
+    chosenHandler(data, (statusCode, payload) => {
+      // Usar o status code called back pelo o handler, ou o default 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200
+
+      // Usar o payload called back pelo o handler, ou o default objeto vazio
+      payload = typeof(payload) == 'object' ? payload : {}
+
+      // Converter o payload para string
+      const payloadString = JSON.stringify(payload)
+
+      // Retornar a resposta
+      res.writeHead(statusCode)
+
+      // Mandar response
+      res.end(payloadString)
+      
+      // Log a response
+      console.log(`Retornando a resposta:`, statusCode,payloadString)
+    })
+
   })
-
-  
-  
 })
 
+// Startar o server, e fazê-lo ouvir a porta 3000
 server.listen(3000, () => {
   console.log("O server está escutando a porta 3000")  
 })
 
-// Startar o server, e fazê-lo ouvir a porta 3000
+// Definir handlers
+const handlers = {}
+
+// Sample Handler
+handlers.sample = function(data, callback) {
+  // Callback um http status code, e um payload object
+  callback(406, {'name': 'sample handler'})
+}
+// Not found handler
+handlers.notFound = function(data, callback) {
+  callback(404)
+}
+
+// Definir os request routers
+const router = {
+  'sample': handlers.sample
+}
