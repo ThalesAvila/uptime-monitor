c480baaf22b04e3c376090de815afc053b092534
Thales Avila <thalespizza@gmail.com>
2019-02-28 20:06:38 -0300

    Funcao tokens.post implementada

--- index.js
+++ index.js
@@ -101,5 +101,6 @@ const unifiedServer = function(req, res) {
 // Definir o request routers
 const router = {
   'ping': handlers.ping,
-  'users': handlers.users
+  'users': handlers.users,
+  'tokens': handlers.tokens
 }
